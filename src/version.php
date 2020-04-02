55e73b321e1b6591b59337d62a933739bc75e645
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-04-02 13:47:27 -0400

    r6

--- src/version.php
+++ src/version.php
@@ -19,13 +19,13 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2020022901;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2020040100;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018050800;        // Requires this Moodle version
 $plugin->component = 'filter_recitcahiercanada'; // Full name of the plugin (used for diagnostics)
 // This is a list of plugins, this plugin depends on (and their versions).                                                          
 $plugin->dependencies = [                                                                                                           
-    'local_recitcommon' => '2020022900', // ANY_VERSION
-	'mod_recitcahiercanada' => '2020022900'
+    'local_recitcommon' => 2020040100, // ANY_VERSION
+	'mod_recitcahiercanada' => 2020040100
 ];
-$plugin->release = 'R5-2020022902'; 
+$plugin->release = 'R6-2020040100'; 
 $plugin->maturity = MATURITY_BETA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
