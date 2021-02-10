8594f596d2ca0e482c7811eda85861d8a79bca8a
Gustavo Bazzo <gcbazzo@gmail.com>
2021-02-10 16:18:17 -0500

    Update version.php

--- src/version.php
+++ src/version.php
@@ -19,13 +19,13 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2020103000;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2021021000;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018050800;        // Requires this Moodle version
 $plugin->component = 'filter_recitcahiercanada'; // Full name of the plugin (used for diagnostics)
 // This is a list of plugins, this plugin depends on (and their versions).                                                          
 $plugin->dependencies = [                                                                                                           
-    'local_recitcommon' => 2020103000, // ANY_VERSION
-	'mod_recitcahiercanada' => 2020103000
+    'local_recitcommon' => 2021021000, // ANY_VERSION
+	'mod_recitcahiercanada' => 2021021000
 ];
-$plugin->release = 'R11-V1.11.0'; 
+$plugin->release = 'R12-V1.12.0'; 
 $plugin->maturity = MATURITY_BETA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
