2f058befeef1eddf85b565875ab654e3d9ebd367
Gustavo Bazzo <gcbazzo@gmail.com>
2022-11-02 11:01:03 -0400

    Create zip with plugin version
    Update version

--- src/version.php
+++ src/version.php
@@ -20,13 +20,13 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022100100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2022100101;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'filter_recitcahiertraces'; // Full name of the plugin (used for diagnostics)
 // This is a list of plugins, this plugin depends on (and their versions).                                                          
 $plugin->dependencies = [
 	'mod_recitcahiertraces' => 2022020900
 ];
-$plugin->release = 'v2.0.3-stable'; 
+$plugin->release = 'v2.1.0-stable'; 
 $plugin->supported = [39, 400];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported. 
 $plugin->maturity = MATURITY_STABLE; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
